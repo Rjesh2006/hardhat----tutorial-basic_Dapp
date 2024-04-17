a9f7c91e55ce4026e129a3220f301a9e9d9a1ba6
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2024-04-17 19:57:53 +0530

    Update and rename modules_Token_js.md to modules_Token.js

---
+++ modules_Token.js
@@ -0,0 +1,10 @@
+
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const TokenModule = buildModule("TokenModule", (m) => {
+  const token = m.contract("Token");
+
+  return { token };
+});
+
+module.exports = TokenModule;
